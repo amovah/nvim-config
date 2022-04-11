0622f971a1a33988e8699e2b61a9df62074725ce
Ali Movahedi <movahedi.4li@gmail.com>
2022-04-11 15:24:00 +0430

    add sqls lsp

--- lsp/configs.lua
+++ lsp/configs.lua
@@ -114,7 +114,7 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 local lspconfig = require('lspconfig')
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = {'gopls', 'rust_analyzer', 'tsserver', 'eslint'}
+local servers = {'gopls', 'rust_analyzer', 'tsserver', 'eslint', 'sqls'}
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
         on_attach = on_attach,
