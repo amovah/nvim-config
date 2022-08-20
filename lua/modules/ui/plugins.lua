46e9e450c0dbcb9ee007a2bb20959a17f143d0c6
Ali Movahedi <ali_movahedi@aol.com>
2022-08-20 11:35:28 +0430

    add basics config

---
+++ lua/modules/ui/plugins.lua
@@ -0,0 +1,22 @@
+local plugin = require('core.pack').register_plugin
+local conf = require('modules.ui.config')
+
+plugin({ 'glepnir/zephyr-nvim', config = conf.zephyr })
+
+plugin({ 'glepnir/dashboard-nvim', config = conf.dashboard })
+
+plugin({
+  'glepnir/galaxyline.nvim',
+  branch = 'main',
+  config = conf.galaxyline,
+  requires = 'kyazdani42/nvim-web-devicons',
+})
+
+plugin({
+  'kyazdani42/nvim-tree.lua',
+  cmd = 'NvimTreeToggle',
+  config = conf.nvim_tree,
+  requires = 'kyazdani42/nvim-web-devicons',
+})
+
+plugin({ 'akinsho/nvim-bufferline.lua', config = conf.nvim_bufferline, requires = 'kyazdani42/nvim-web-devicons' })
