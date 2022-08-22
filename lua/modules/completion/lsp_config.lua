e0fba174c01a3a51b021245029b499313887d16f
Ali Movahedi <ali_movahedi@aol.com>
2022-08-22 15:23:03 +0430

    add some lsp

--- lua/modules/completion/lsp_config.lua
+++ lua/modules/completion/lsp_config.lua
@@ -1,23 +1,53 @@
 local on_attach = function(client, bufnr)
   require('lsp_signature').on_attach({
-      bind = true, -- This is mandatory, otherwise border config won't get registered.
-      handler_opts = {
-        border = "rounded"
-      }
-    }, bufnr)
+    bind = true, -- This is mandatory, otherwise border config won't get registered.
+    handler_opts = {
+      border = "rounded"
+    }
+  }, bufnr)
   require('illuminate').on_attach(client)
 end
 
 local lsp_config = function()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
   local lspconfig = require('lspconfig')
-
-  lspconfig['gopls'].setup {
+  local common_lspconfig = {
     on_attach = on_attach,
-    capabilities = capabilities
+    capabilities = capabilities,
   }
+
+  local lsp_servers = {
+    'gopls',
+    'rust_analyzer',
+    'tsserver',
+    'eslint',
+    'sqls',
+    'dockerls',
+    'cssls',
+    'jedi_language_server',
+    'solc',
+    'tailwindcss',
+    'terraformls',
+    'vimls',
+  }
+
+  for _, lsp in ipairs(lsp_servers) do
+    lspconfig[lsp].setup(common_lspconfig)
+  end
+
+  lspconfig['sumneko_lua'].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { 'vim' }
+        }
+      }
+    }
+  })
+
 end
 
 return lsp_config
