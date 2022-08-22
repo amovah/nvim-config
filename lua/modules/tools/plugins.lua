a4c3ef81074a3fcab37f5047fa86c02afc7f3dd4
Ali Movahedi <ali_movahedi@aol.com>
2022-08-22 10:28:14 +0430

    add telescope plugin

---
+++ lua/modules/tools/plugins.lua
@@ -0,0 +1,13 @@
+local plugin = require('core.pack').register_plugin
+local conf = require('modules.tools.config')
+
+plugin({
+  'nvim-telescope/telescope.nvim',
+  cmd = 'Telescope',
+  config = conf.telescope,
+  requires = {
+    'nvim-lua/popup.nvim',
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope-fzy-native.nvim',
+  },
+})
