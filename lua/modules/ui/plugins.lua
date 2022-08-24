e66833cc5bac37f15c3f6502273e5383904eb040
Ali Movahedi <ali_movahedi@aol.com>
2022-08-24 13:23:01 +0430

    add couple themes

--- lua/modules/ui/plugins.lua
+++ lua/modules/ui/plugins.lua
@@ -1,11 +1,71 @@
 local plugin = require('core.pack').register_plugin
 local conf = require('modules.ui.config')
 
--- themes
+--#region theme
+local themes = {
+	'ellisonleao/gruvbox.nvim',
+	'Abstract-IDE/Abstract-cs',
+	'rafamadriz/neon',
+	'tomasiser/vim-code-dark',
+	'marko-cerovac/material.nvim',
+	'bluz71/vim-nightfly-guicolors',
+	'bluz71/vim-moonfly-colors',
+	'folke/tokyonight.nvim',
+	'sainnhe/sonokai',
+	'kyazdani42/blue-moon',
+	'mhartington/oceanic-next',
+	'rockerBOO/boo-colorscheme-nvim',
+	'sainnhe/edge',
+	'bkegley/gloombuddy',
+	'Th3Whit3Wolf/space-nvim',
+	'Th3Whit3Wolf/one-nvim',
+	'yonlu/omni.vim',
+	'ray-x/aurora',
+	'tanvirtin/monokai.nvim',
+	'savq/melange',
+	'fenetikm/falcon',
+	'shaunsingh/nord.nvim',
+	'shaunsingh/moonlight.nvim',
+	'navarasu/onedark.nvim',
+	'lourenci/github-colors',
+	'sainnhe/gruvbox-material',
+	'sainnhe/everforest',
+	'NTBBloodbath/doom-one.nvim',
+	'yashguptaz/calvera-dark.nvim',
+	'nxvu699134/vn-night.nvim',
+	'adisen99/codeschool.nvim',
+	'projekt0n/github-nvim-theme',
+	'kdheepak/monochrome.nvim',
+	'rose-pine/neovim',
+	'mcchrish/zenbones.nvim',
+	'catppuccin/nvim',
+	'FrenzyExists/aquarium-vim',
+	'EdenEast/nightfox.nvim',
+	'kvrohit/substrata.nvim',
+	'ldelossa/vimdark',
+	'Everblush/everblush.nvim',
+	'olimorris/onedarkpro.nvim',
+	'rmehri01/onenord.nvim',
+	'RishabhRD/gruvy',
+	'rebelot/kanagawa.nvim',
+	'luisiacc/gruvbox-baby',
+	'tiagovla/tokyodark.nvim',
+	'kvrohit/rasmus.nvim',
+	'yazeed1s/minimal.nvim',
+	'lewpoly/sherbet.nvim',
+	'Mofiqul/adwaita.nvim',
+	'olivercederborg/poimandres.nvim',
+}
 plugin({
 	'glepnir/zephyr-nvim',
 	config = conf.zephyr,
 })
+for _, theme in pairs(themes) do
+	plugin({
+		theme,
+	})
+end
+--#endregion
 
 plugin({
 	'glepnir/dashboard-nvim',
