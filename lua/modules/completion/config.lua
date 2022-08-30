eb9135786a47eab930de966691356199768487c9
Ali Movahedi <ali_movahedi@aol.com>
2022-08-30 12:25:22 +0430

    add bashls and yamlls

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -1,36 +1,38 @@
 local config = {}
 
 function config.lspsaga()
-	local saga = require('lspsaga')
-	saga.init_lsp_saga({
-		-- symbol_in_winbar = {
-		--   enable = true,
-		-- },
-	})
+  local saga = require('lspsaga')
+  saga.init_lsp_saga({
+    -- symbol_in_winbar = {
+    --   enable = true,
+    -- },
+  })
 end
 
 function config.mason()
-	require('mason').setup({})
+  require('mason').setup({})
 end
 
 function config.mason_lspconfig()
-	require("mason-lspconfig").setup({
-		ensure_installed = {
-			'gopls',
-			'rust_analyzer',
-			'tsserver',
-			'eslint',
-			'sqls',
-			'dockerls',
-			'cssls',
-			'jedi_language_server',
-			'solc',
-			'tailwindcss',
-			'terraformls',
-			'vimls',
-			'sumneko_lua',
-		}
-	})
+  require("mason-lspconfig").setup({
+    ensure_installed = {
+      "gopls",
+      "rust_analyzer",
+      "tsserver",
+      "eslint",
+      "sqls",
+      "dockerls",
+      "cssls",
+      "jedi_language_server",
+      "solc",
+      "tailwindcss",
+      "terraformls",
+      "vimls",
+      "sumneko_lua",
+      "yamlls",
+      "bashls",
+    }
+  })
 end
 
 return config
--- lua/modules/completion/lsp_config.lua
+++ lua/modules/completion/lsp_config.lua
@@ -1,48 +1,55 @@
 local on_attach = function(client, bufnr)
-	require('lsp_signature').on_attach({
-		bind = true, -- This is mandatory, otherwise border config won't get registered.
-		handler_opts = {
-			border = "rounded"
-		}
-	}, bufnr)
-	require('illuminate').on_attach(client)
+  require("lsp_signature").on_attach({
+    bind = true, -- This is mandatory, otherwise border config won't get registered.
+    handler_opts = {
+      border = "rounded"
+    }
+  }, bufnr)
+  require("illuminate").on_attach(client)
 end
 
 local lsp_config = function()
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-	local lspconfig = require('lspconfig')
-	local server_config = {
-		on_attach = on_attach,
-		capabilities = capabilities,
-		settings = {
-			Lua = {
-				diagnostics = {
-					globals = { 'vim' }
-				}
-			}
-		}
-	}
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+  local lspconfig = require("lspconfig")
+  local server_config = {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { "vim" }
+        }
+      },
+      yaml = {
+        schemas = {
+          kubernetes = "globPattern",
+        },
+      },
+    },
+  }
 
-	local lsp_servers = {
-		'gopls',
-		'rust_analyzer',
-		'tsserver',
-		'eslint',
-		'sqls',
-		'dockerls',
-		'cssls',
-		'jedi_language_server',
-		'solc',
-		'tailwindcss',
-		'terraformls',
-		'vimls',
-		'sumneko_lua',
-	}
+  local lsp_servers = {
+    "gopls",
+    "rust_analyzer",
+    "tsserver",
+    "eslint",
+    "sqls",
+    "dockerls",
+    "cssls",
+    "jedi_language_server",
+    "solc",
+    "tailwindcss",
+    "terraformls",
+    "vimls",
+    "sumneko_lua",
+    "yamlls",
+    "bashls",
+  }
 
-	for _, lsp in ipairs(lsp_servers) do
-		lspconfig[lsp].setup(server_config)
-	end
+  for _, lsp in ipairs(lsp_servers) do
+    lspconfig[lsp].setup(server_config)
+  end
 end
 
 return lsp_config
