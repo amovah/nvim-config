8baeac0b567a02bc65505c2570d4d1c86e7eed6a
Ali Movahedi <ali_movahedi@aol.com>
2022-08-31 09:55:13 +0430

    style: relint lua files

--- lua/modules/lang/plugins.lua
+++ lua/modules/lang/plugins.lua
@@ -2,67 +2,67 @@ local plugin = require('core.pack').register_plugin
 local conf = require('modules.lang.config')
 
 plugin({
-	'nvim-treesitter/nvim-treesitter',
-	event = 'BufRead',
-	run = ':TSUpdate',
-	after = 'telescope.nvim',
-	config = conf.nvim_treesitter,
-	requires = {
-		{ 'nvim-treesitter/nvim-treesitter-textobjects', after = 'nvim-treesitter' },
-		{ 'windwp/nvim-ts-autotag', after = 'nvim-treesitter' },
-		-- disabled due to conflict with matchup and it doesn't provide any
-		-- highlighting (not my personal prefer)
-		-- { 'p00f/nvim-ts-rainbow', after = 'nvim-treesitter' },
-		{ 'andymass/vim-matchup', after = 'nvim-treesitter' },
-	},
+  'nvim-treesitter/nvim-treesitter',
+  event = 'BufRead',
+  run = ':TSUpdate',
+  after = 'telescope.nvim',
+  config = conf.nvim_treesitter,
+  requires = {
+    { 'nvim-treesitter/nvim-treesitter-textobjects', after = 'nvim-treesitter' },
+    { 'windwp/nvim-ts-autotag', after = 'nvim-treesitter' },
+    -- disabled due to conflict with matchup and it doesn't provide any
+    -- highlighting (not my personal prefer)
+    -- { 'p00f/nvim-ts-rainbow', after = 'nvim-treesitter' },
+    { 'andymass/vim-matchup', after = 'nvim-treesitter' },
+  },
 })
 
 plugin({
-	'nvim-treesitter/nvim-treesitter-context',
-	after = 'nvim-treesitter',
-	config = conf.nvim_treesitter_context,
+  'nvim-treesitter/nvim-treesitter-context',
+  after = 'nvim-treesitter',
+  config = conf.nvim_treesitter_context,
 })
 
 plugin({
-	'ThePrimeagen/refactoring.nvim',
-	opt = true,
-	config = conf.refactoring,
-	after = { 'nvim-treesitter' }
+  'ThePrimeagen/refactoring.nvim',
+  opt = true,
+  config = conf.refactoring,
+  after = { 'nvim-treesitter' }
 })
 
 plugin({
-	'kevinhwang91/nvim-ufo',
-	after = 'nvim-treesitter',
-	requires = { "kevinhwang91/promise-async" },
-	config = conf.ufo,
+  'kevinhwang91/nvim-ufo',
+  after = 'nvim-treesitter',
+  requires = { "kevinhwang91/promise-async" },
+  config = conf.ufo,
 })
 
 plugin({
-	'simrat39/symbols-outline.nvim',
-	opt = true,
-	cmd = {
-		'SymbolsOutline',
-		'SymbolsOutlineClose',
-		'SymbolsOutlineOpen'
-	},
-	config = conf.symbols_outline,
+  'simrat39/symbols-outline.nvim',
+  opt = true,
+  cmd = {
+    'SymbolsOutline',
+    'SymbolsOutlineClose',
+    'SymbolsOutlineOpen'
+  },
+  config = conf.symbols_outline,
 })
 
 plugin({
-	'folke/trouble.nvim',
-	opt = true,
-	cmd = { 'Trouble' },
-	config = conf.trouble,
+  'folke/trouble.nvim',
+  opt = true,
+  cmd = { 'Trouble' },
+  config = conf.trouble,
 })
 
 plugin({
-	'sbdchd/neoformat',
-	opt = true,
-	cmd = { 'Neoformat' },
+  'sbdchd/neoformat',
+  opt = true,
+  cmd = { 'Neoformat' },
 })
 
 plugin({
-	'buoto/gotests-vim',
-	opt = true,
-	cmd = { 'GoTests', 'GoTestsAll' }
+  'buoto/gotests-vim',
+  opt = true,
+  cmd = { 'GoTests', 'GoTestsAll' }
 })
--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -1,83 +1,83 @@
 local config = {}
 
 function config.telescope()
-	require('telescope').setup({
-		defaults = {
-			layout_config = {
-				horizontal = { prompt_position = 'top', results_width = 0.6 },
-				vertical = { mirror = false },
-			},
-			sorting_strategy = 'ascending',
-			file_previewer = require('telescope.previewers').vim_buffer_cat.new,
-			grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
-			qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
-		},
-		extensions = {
-			fzy_native = {
-				override_generic_sorter = false,
-				override_file_sorter = true,
-			},
-		},
-	})
+  require('telescope').setup({
+    defaults = {
+      layout_config = {
+        horizontal = { prompt_position = 'top', results_width = 0.6 },
+        vertical = { mirror = false },
+      },
+      sorting_strategy = 'ascending',
+      file_previewer = require('telescope.previewers').vim_buffer_cat.new,
+      grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
+      qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+    },
+    extensions = {
+      fzy_native = {
+        override_generic_sorter = false,
+        override_file_sorter = true,
+      },
+    },
+  })
 
-	require('telescope').load_extension('fzy_native')
-	require("telescope").load_extension('harpoon')
+  require('telescope').load_extension('fzy_native')
+  require("telescope").load_extension('harpoon')
 end
 
 function config.gitsigns()
-	require('gitsigns').setup({})
+  require('gitsigns').setup({})
 end
 
 function config.auto_session()
-	require('auto-session').setup({
-		bypass_session_save_file_types = { 'dashboard' },
-		log_level = 'error',
-	})
+  require('auto-session').setup({
+    bypass_session_save_file_types = { 'dashboard' },
+    log_level = 'error',
+  })
 end
 
 function config.autopairs()
-	require('nvim-autopairs').setup({})
+  require('nvim-autopairs').setup({})
 end
 
 function config.comment()
-	require('Comment').setup({})
+  require('Comment').setup({})
 end
 
 function config.indent_blankline()
-	require("indent_blankline").setup({})
+  require("indent_blankline").setup({})
 end
 
 function config.colorizer()
-	require('colorizer').setup({})
+  require('colorizer').setup({})
 end
 
 function config.twilight()
-	require('twilight').setup({})
+  require('twilight').setup({})
 end
 
 function config.editorconfig()
-	vim.g.EditorConfig_exclude_patterns = { 'fugitive://.*', 'scp://.*' }
+  vim.g.EditorConfig_exclude_patterns = { 'fugitive://.*', 'scp://.*' }
 end
 
 function config.neoclip()
-	require('neoclip').setup({})
-	require("telescope").load_extension("neoclip")
+  require('neoclip').setup({})
+  require("telescope").load_extension("neoclip")
 end
 
 function config.true_zen()
-	require('true-zen').setup({})
+  require('true-zen').setup({})
 end
 
 function config.rest()
-	require('rest-nvim').setup({})
+  require('rest-nvim').setup({})
 end
 
 function config.git_conflict()
-	require('git-conflict').setup({})
+  require('git-conflict').setup({})
 end
 
 function config.leap()
-	require('leap').set_default_keymaps()
+  require('leap').set_default_keymaps()
 end
 
 return config
