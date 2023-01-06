2a7055c0eaa020d738ef2f45d9c0dc6684691714
Ali Movahedi <movahedi.4li@gmail.com>
2023-01-06 13:19:05 +0330

    add emoji

--- lua/modules/ui/evil_lualine.lua
+++ lua/modules/ui/evil_lualine.lua
@@ -58,7 +58,7 @@
 -- })
 
 local function full_file_path()
-	return vim.fn.fnamemodify(vim.fn.getcwd(), ":t") .. "->" .. vim.fn.pathshorten(vim.fn.expand("%:."))
+	return vim.fn.fnamemodify(vim.fn.getcwd(), ":t") .. " 💻 " .. vim.fn.pathshorten(vim.fn.expand("%:."))
 end
 
 require("lualine").setup({
