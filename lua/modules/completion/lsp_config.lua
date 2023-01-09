204881fe613add197168f561531c49b4cf93c5b7
Ali Movahedi <movahedi.4li@gmail.com>
2023-01-09 09:05:32 +0330

    remove diagnostic for protolint

--- lua/modules/completion/lsp_config.lua
+++ lua/modules/completion/lsp_config.lua
@@ -114,7 +114,6 @@ function M.null_ls()
 	local null_ls = require("null-ls")
 	null_ls.setup({
 		sources = {
-			null_ls.builtins.diagnostics.protolint,
 			null_ls.builtins.diagnostics.eslint_d,
 			null_ls.builtins.formatting.prettierd,
 			null_ls.builtins.formatting.eslint_d,
