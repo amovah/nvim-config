c42ff3b49dc66f81dc32600c7992e6688c204404
Ali Movahedi <movahedi.4li@gmail.com>
2023-11-16 12:50:50 +0330

    chore: remove unused function

--- lua/modules/completion/lsp_config.lua
+++ lua/modules/completion/lsp_config.lua
@@ -1,21 +1,5 @@
 local M = {}
 
-local org_imports = function(wait_ms)
-	print("org imports file")
-	local params = vim.lsp.util.make_range_params()
-	params.context = { only = { "source.organizeImports" } }
-	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-	for _, res in pairs(result or {}) do
-		for _, r in pairs(res.result or {}) do
-			if r.edit then
-				vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
-			else
-				vim.lsp.buf.execute_command(r.command)
-			end
-		end
-	end
-end
-
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local on_attach = function(client, bufnr)
 	if client.server_capabilities.documentFormattingProvider then
