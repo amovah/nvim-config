d66310e4cc7ba61679518d467ff57227ee95ef5b
Ali Movahedi <movahedi.4li@gmail.com>
2023-11-16 12:50:05 +0330

    fix: update go imports

--- lua/modules/completion/lsp_config.lua
+++ lua/modules/completion/lsp_config.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 local org_imports = function(wait_ms)
+	print("org imports file")
 	local params = vim.lsp.util.make_range_params()
 	params.context = { only = { "source.organizeImports" } }
 	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
@@ -32,15 +33,28 @@ local on_attach = function(client, bufnr)
 			end,
 		})
 
-		if client.name == "gopls" then
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					org_imports(1000)
-				end,
-			})
-		end
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*.go",
+			callback = function()
+				local params = vim.lsp.util.make_range_params()
+				params.context = { only = { "source.organizeImports" } }
+				-- buf_request_sync defaults to a 1000ms timeout. Depending on your
+				-- machine and codebase, you may want longer. Add an additional
+				-- argument after params if you find that you have to write the file
+				-- twice for changes to be saved.
+				-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+				local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+				for cid, res in pairs(result or {}) do
+					for _, r in pairs(res.result or {}) do
+						if r.edit then
+							local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+							vim.lsp.util.apply_workspace_edit(r.edit, enc)
+						end
+					end
+				end
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
 	end
 
 	require("lsp_signature").on_attach({
