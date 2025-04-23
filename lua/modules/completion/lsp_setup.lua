d1f8ae3302aadc2a889115b49243edeb579df439
Ali Movahedi <movahedi.4li@gmail.com>
2025-04-23 10:36:42 +0330

    chore: remove comments

--- lua/modules/completion/lsp_setup.lua
+++ lua/modules/completion/lsp_setup.lua
@@ -27,11 +27,6 @@ local lsp_config = function()
 		callback = function()
 			local params = vim.lsp.util.make_range_params()
 			params.context = { only = { "source.organizeImports" } }
-			-- buf_request_sync defaults to a 1000ms timeout. Depending on your
-			-- machine and codebase, you may want longer. Add an additional
-			-- argument after params if you find that you have to write the file
-			-- twice for changes to be saved.
-			-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
 			local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
 			for cid, res in pairs(result or {}) do
 				for _, r in pairs(res.result or {}) do
@@ -49,72 +44,15 @@ local lsp_config = function()
 		virtual_text = true,
 	})
 
-	-- vim.api.nvim_create_autocmd("LspAttach", {
-	-- 	group = vim.api.nvim_create_augroup("buf_behavior_conf", { clear = true }),
-	-- 	callback = function(event_context)
-	-- 		local client = vim.lsp.get_client_by_id(event_context.data.client_id)
-	--
-	-- 		if not client then
-	-- 			return
-	-- 		end
-	--
-	-- 		local bufnr = event_context.buf
-	--
-	-- 		if client.server_capabilities.documentHighlightProvider then
-	-- 			local gid = api.nvim_create_augroup("lsp_document_highlight", { clear = true })
-	-- 			api.nvim_create_autocmd("CursorHold", {
-	-- 				group = gid,
-	-- 				buffer = bufnr,
-	-- 				callback = function()
-	-- 					lsp.buf.document_highlight()
-	-- 				end,
-	-- 			})
-	--
-	-- 			api.nvim_create_autocmd("CursorMoved", {
-	-- 				group = gid,
-	-- 				buffer = bufnr,
-	-- 				callback = function()
-	-- 					lsp.buf.clear_references()
-	-- 				end,
-	-- 			})
-	-- 		end
-	-- 	end,
-	-- 	nested = true,
-	-- 	desc = "Configure buffer keymap and behavior based on LSP",
-	-- })
-
 	local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-	-- For what diagnostic is enabled in which type checking mode, check doc:
-	-- https://github.com/microsoft/pyright/blob/main/docs/configuration.md#diagnostic-settings-defaults
-	-- Currently, the pyright also has some issues displaying hover documentation:
-	-- https://www.reddit.com/r/neovim/comments/1gdv1rc/what_is_causeing_the_lsp_hover_docs_to_looks_like/
-
 	if utils.executable("pyright") then
-		-- local new_capability = {
-		-- 	-- this will remove some of the diagnostics that duplicates those from ruff, idea taken and adapted from
-		-- 	-- here: https://github.com/astral-sh/ruff-lsp/issues/384#issuecomment-1989619482
-		-- 	textDocument = {
-		-- 		publishDiagnostics = {
-		-- 			tagSupport = {
-		-- 				valueSet = { 2 },
-		-- 			},
-		-- 		},
-		-- 		hover = {
-		-- 			contentFormat = { "plaintext" },
-		-- 			dynamicRegistration = true,
-		-- 		},
-		-- 	},
-		-- }
-		-- local merged_capability = vim.tbl_deep_extend("force", capabilities, new_capability)
-
 		lspconfig.pyright.setup({
 			cmd = { "delance-langserver", "--stdio" },
-			capabilities = merged_capability,
+			capabilities = capabilities,
 			settings = {
 				pyright = {
-					-- disable import sorting and use Ruff for this
-					disableOrganizeImports = true,
+					disableOrganizeImports = false,
 					disableTaggedHints = false,
 				},
 				python = {
@@ -123,16 +61,8 @@ local lsp_config = function()
 						diagnosticMode = "workspace",
 						typeCheckingMode = "standard",
 						useLibraryCodeForTypes = true,
-						-- we can this setting below to redefine some diagnostics
 						diagnosticSeverityOverrides = {
 							deprecateTypingAliases = false,
-						},
-						-- inlay hint settings are provided by pylance?
-						inlayHints = {
-							callArgumentNames = "partial",
-							functionReturnTypes = true,
-							pytestParameters = true,
-							variableTypes = true,
 						},
 					},
 				},
@@ -214,7 +144,10 @@ local lsp_config = function()
 		})
 	end
 
-	require("lsp_signature").setup({})
+	require("lsp_signature").setup({
+		bind = true,
+		-- hint_enable = false,
+	})
 end
 
 return lsp_config
